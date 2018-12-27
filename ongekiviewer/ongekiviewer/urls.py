c97e3430636c221aaa43ba0c34d83dc516133488
ebina <ebina@ebinanoMacBook-puro.local>
2018-12-27 17:31:34 +0900

    TOPページ実装

--- ongekiviewer/ongekiviewer/urls.py
+++ ongekiviewer/ongekiviewer/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from ongekiviewer import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('baseratelist/', include('baseratelist.urls')),
     path('admin/', admin.site.urls),
 ]
