65125cab57cf5b9028c59a64264e07727c629b4f
rate0621 <rate@localhost.localdomain>
2019-02-25 22:08:06 +0900

    微修正

--- ongekiviewer/ongekiviewer/settings.py
+++ ongekiviewer/ongekiviewer/settings.py
@@ -18,8 +18,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Quick-start development settings - unsuitable for production
