70084459343002b314a5eeff6dab25111f266e21
ebina <ebina@ebinanoMacBook-puro.local>
2019-02-25 15:06:35 +0900

    mod_wsgi対応

--- ongekiviewer/ongekiviewer/wsgi.py
+++ ongekiviewer/ongekiviewer/wsgi.py
@@ -9,13 +9,16 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
-
 ## rate add
 from django.core.wsgi import get_wsgi_application
-
-
 from dj_static import Cling
 
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ongekiviewer.settings")
+
 
 application = Cling(get_wsgi_application())
